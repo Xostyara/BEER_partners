6bb11630484f4a01afc740dcf7f39109efd63812
Xostyara <xostyara.gmail.com>
2024-07-24 00:14:54 +0300

    Описаны модели и настройки админки, нужно сделать связи между таблицами

--- BEERS/beer_menu/admin.py
+++ BEERS/beer_menu/admin.py
@@ -1,3 +1,62 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Beer, Provider, Manufacturer
+
+class ProviderAdmin(admin.ModelAdmin):
+    # Перечисляем поля, которые должны отображаться в админке
+    list_display = ('pk', 
+                    'name_provider',
+                    'INN',
+                    'number_of_agreement',
+                    )
+    search_fields = ('name_provider',
+                    'INN',
+                    'number_of_agreement',
+                    ) 
+
+
+class ManufacturerAdmin(admin.ModelAdmin):
+    
+    list_display = ('pk', 
+                    'name_manufacturer',
+                    'country',
+                    'city',
+                    )
+    search_fields = ('name_manufacturer',
+                    'country',
+                    'city',
+                    ) 
+    
+
+class BeerAdmin(admin.ModelAdmin):
+    # Перечисляем поля, которые должны отображаться в админке
+    list_display = ('pk',
+                    'name', 
+                    'type_beer', 
+                    'provider', 
+                    'manufacturer', 
+                    'description',
+                    'density',
+                    'fortress',
+                    'color',
+                    'bitterness',
+                    'alcohol',
+                    'taste',
+                    'date_publication',
+                    'photo',
+                    'active',
+                    ) 
+    # Добавляем интерфейс для поиска по тексту постов
+    search_fields = ('name', 
+                    'type_beer',
+                    'manufacturer'
+                    ) 
+    # Добавляем возможность фильтрации по дате
+    list_filter = ('name',) 
+
+
+admin.site.register(Provider, ProviderAdmin) 
+admin.site.register(Manufacturer, ManufacturerAdmin) 
+admin.site.register(Beer, BeerAdmin) 
+
--- BEERS/beer_menu/models.py
+++ BEERS/beer_menu/models.py
@@ -1,47 +1,94 @@
 from django.db import models
 
+from .directorys import BEER_TYPE_CHOICES, GRADE_CHOICES
 # Create your models here.
+
+
+class Provider(models.Model):
+    """Таблица с информацией о поставщике"""
+
+    name_provider = models.CharField(max_length = 100, 
+                                verbose_name='Название компании')
+    INN = models.CharField(max_length=15,
+                       verbose_name='ИНН'
+                       )
+    number_of_agreement = models.CharField(max_length=50,
+                                           verbose_name='Номер договора'
+                                           )
+
+
+class Manufacturer(models.Model):
+    """Таблица с информацией о поставщике"""
+    name_manufacturer = models.CharField(max_length = 100, 
+                                verbose_name='Название производителя')
+    country = models.CharField(max_length = 100, 
+                                verbose_name='Страна')
+    city = models.CharField(max_length = 50, 
+                                verbose_name='Город')
+    
+    
 
 
 class Beer(models.Model): 
     "Таблица с видами пива"
-    name = models.CharField(max_length = 100)
+    name = models.CharField(max_length = 100,
+                            verbose_name='Название'
+                            )
     
-    type_beer= models.CharField(max_length= 20)  # потом сделать связку с таблицей типов пива (
+    type_beer= models.CharField(max_length=1, 
+                               choices=BEER_TYPE_CHOICES,
+                               verbose_name='Тип пива',
+                                )  # потом сделать связку с таблицей типов пива (
     # крафтовое, лагер и т.д. или тянуть из справочника)
     
     # поставщик
-    provider = models.CharField(max_length = 100)   # потом сделать связку с таблицей Поставщиков (для стандартизации)
+    provider = models.CharField(max_length = 100, 
+                                verbose_name='Поставщик'
+                                )   # потом сделать связку с таблицей Поставщиков (для стандартизации)
     
     # производитель
-    manufacturer = models.CharField(max_length = 100)   # потом сделать связку с таблицей Производителей (для стандартизации)
+    manufacturer = models.CharField(max_length = 100,
+                                    verbose_name='Производитель'
+                                    )   # потом сделать связку с таблицей Производителей (для стандартизации)
+    # volume = models.FloatField(verbose_name="Объем в литрах", blank=True)
+    # price = models.CharField(max_length = 5,
+    #                          verbose_name='Цена', 
+    #                          blank=True
+    #                          )
     
-    description = models.TextField()       # описание
+    description = models.TextField(verbose_name='Описание')       
 
-    density = models.TextField() # Плотность
+    density = models.CharField(max_length=1, 
+                               choices=GRADE_CHOICES,
+                               verbose_name='Плотность от 1 до 5'
+                               ) 
 
-    fortress = models.TextField() # крепость
+    fortress = models.TextField(max_length=1, 
+                               choices=GRADE_CHOICES,
+                               verbose_name='Крепость от 1 до 5'
+                               ) 
 
-    color = models.TextField() # Цвет
+    color = models.TextField(max_length=1, 
+                             choices=GRADE_CHOICES,
+                             verbose_name='Цвет от 1 до 5'
+                             ) 
 
-    bitterness = models.TextField() # Горечь
+    bitterness = models.TextField(max_length=1, 
+                                  choices=GRADE_CHOICES,
+                                  verbose_name='Горечь от 1 до 5'
+                                  ) 
 
-    fortress_in_procent = models.TextField()  # Крепость в процентах
+    alcohol = models.TextField(verbose_name='Алкоголь в %') 
 
-    taste = models.TextField()  # вкус
+    taste = models.TextField(verbose_name='Описание вкуса')  
 
-    date_publication = models.DateField(auto_now_add=True)  # дата создания
+    date_publication = models.DateField(auto_now_add=True, 
+                                        verbose_name="дата создания"
+                                        )  
 
-    photo = models.ImageField()   # фотография/картинка
+    photo = models.ImageField(verbose_name='Фото')   # фотография/картинка
     
-#     1) название
-# 2) поставщик
-# 3) производитель
-# 4) описание
-# 5) характеристики 
-#     а) Плотность 
-#     б) Крепость 
-#     в) Цвет 
-#     г) Горечь
-# 6) крепость (алкоголь в %) 
-# 7) ВКУС - описание 
+    active = models.BooleanField(default= False, 
+                                verbose_name='В наличии'
+                                ) #  - true/false
+    
